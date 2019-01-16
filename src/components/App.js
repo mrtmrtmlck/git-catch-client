d1f9bc929ef37eda7d97259c0634b716455e6913
gizemkeser <gzemkeser@gmail.com>
2019-01-17 00:55:17 +0300

    Modify project structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Github Issue Catcher
+      </div>
+    );
+  }
+}
+
+export default App;
