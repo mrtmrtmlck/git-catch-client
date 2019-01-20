0dcc1292d0fb4d04ff30b3761adccf3138759a86
gizemkeser <gzemkeser@gmail.com>
2019-01-20 03:10:03 +0300

    Change subscription mutation

--- src/components/SubsciptionForm.js
+++ src/components/SubsciptionForm.js
@@ -43,47 +43,56 @@ class SubscriptionForm extends Component {
     }
 
     handleSelectComponentChange = (e, listName) => {
-        // This logic will change after the new component added
+        // Change this logic after the new select component added
         const val = e.target.value
         const list = this.state[listName]
         this.setState({ [listName]: [...list, val] })
     }
 
-    render() {
-        const { email, labelIdList, languageIdList } = this.state
+    onFormSubmit = (e, subscribeUser) => {
+        e.preventDefault();
+        subscribeUser({ variables: this.state })
+        this.setState({
+            email: '',
+            labelIdList: [],
+            languageIdList: []
+        })
+        // Show message to the user and clean the form
+    }
 
+    render() {
+        const { email } = this.state
         return (
-            <form className="subscriptionForm">
-                <div className="formField">
-                    <span>Email:</span>
-                    <input type="email"
-                        name="email"
-                        value={email}
-                        onChange={this.onEmailChange} />
-                </div>
-                <div className="formField">
-                    <span>Label:</span>
-                    <SelectComponent
-                        name="labels"
-                        query={QUERY_LABEL}
-                        handleChange={ e => this.handleSelectComponentChange(e, 'labelIdList')}></SelectComponent>
-                </div>
-                <div className="formField">
-                    <span>Language:</span>
-                    <SelectComponent
-                        name="languages"
-                        query={QUERY_LANGUAGE}
-                        handleChange={ e => this.handleSelectComponentChange(e, 'languageIdList')}></SelectComponent>
-                </div>
-                <div className="formField">
-                    <Mutation mutation={MUTATION_SUBSCRIBE} variables={{ email, labelIdList, languageIdList }}>
-                        {postMutation => (
-                            <button type="submit"
-                                onClick={postMutation}>Subscribe</button>
-                        )}
-                    </Mutation>
-                </div>
-            </form>
+            <Mutation mutation={MUTATION_SUBSCRIBE}>
+                { subscribeUser => (
+                    <form className="subscriptionForm" onSubmit={ e => this.onFormSubmit(e, subscribeUser)}>
+                        <div className="formField">
+                            <span>Email:</span>
+                            <input type="email"
+                                name="email"
+                                value={email}
+                                onChange={this.onEmailChange} />
+                        </div>
+                        <div className="formField">
+                            <span>Label:</span>
+                            <SelectComponent
+                                name="labels"
+                                query={QUERY_LABEL}
+                                handleChange={ e => this.handleSelectComponentChange(e, 'labelIdList')}></SelectComponent>
+                        </div>
+                        <div className="formField">
+                            <span>Language:</span>
+                            <SelectComponent
+                                name="languages"
+                                query={QUERY_LANGUAGE}
+                                handleChange={ e => this.handleSelectComponentChange(e, 'languageIdList')}></SelectComponent>
+                        </div>
+                        <div className="formField">
+                            <button type="submit">Subscribe</button>
+                        </div>
+                    </form>
+                )}
+            </Mutation>
         )
     }
 }
